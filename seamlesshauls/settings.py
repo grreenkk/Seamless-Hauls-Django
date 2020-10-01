11d0ac779646a0c719e563792145cd06e2ecfa1b
kc green <greenakawushim@gmail.com>
2020-10-01 08:58:56 +0100

    Removed p

--- seamlesshauls/settings.py
+++ seamlesshauls/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'greenkelechi@gmail.com'
-EMAIL_HOST_PASSWORD = 'Bracelet12345'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL= False
 
